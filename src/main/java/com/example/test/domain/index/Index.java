bf606f7b93a06923244123f249542f17e56ff31d
JiWonKim <hghg1036@naver.com>
2023-07-02 03:07:30 +0900

    bean 오류 수정완료

---
+++ src/main/java/com/example/test/domain/index/Index.java
@@ -0,0 +1,5 @@
+package com.example.test.domain.index;
+
+public class Index {
+    
+}
--- src/main/java/com/example/test/service/IndexService.java
+++ src/main/java/com/example/test/service/IndexService.java
@@ -1,5 +1,8 @@
 package com.example.test.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class IndexService {
 
 }
